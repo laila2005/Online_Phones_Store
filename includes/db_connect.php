71dad796302ce8d9b5cbcc87844f725d790699da
laila2005 <laila.mf2005@gmail.com>
2025-12-01 23:37:39 +0200

    creating database

---
+++ includes/db_connect.php
@@ -0,0 +1,22 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = ""; // XAMPP default is usually no password
+$dbname = "ecommerce_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    // Stop the script and display an error message if the connection fails
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Optional: Set the character set to UTF-8 for proper handling of all characters
+$conn->set_charset("utf8mb4");
+
+// The $conn variable now holds the active database connection.
+// This file will be included in every PHP file that needs to talk to the database.
+?>
